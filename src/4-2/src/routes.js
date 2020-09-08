ed021efba21c9b61d2413ede5416f533f425afd8
Rafael Prado <contato@rprado.design>
2020-09-08 15:37:17 -0300

    Update: adjust routes

--- src/4-2/src/routes.js
+++ src/4-2/src/routes.js
@@ -1,16 +1,16 @@
-const express = require("express")
-const routes = express.Router()
+const express = require("express");
+const routes = express.Router();
 
-routes.get("/", function(req, res) {
-    return res.redirect("/teachers")
-})
+routes.get("/", function (req, res) {
+  return res.redirect("/teachers");
+});
 
-routes.get("/teachers", function(req, res) {
-    return res.render("teachers")
-})
+routes.get("/teachers", function (req, res) {
+  return res.render("teachers/index");
+});
 
-routes.get("/students", function(req, res) {
-    return res.render("students")
-})
+routes.get("/students", function (req, res) {
+  return res.render("students/index");
+});
 
-module.exports = routes
+module.exports = routes;
