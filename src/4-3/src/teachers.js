e21d245424195f961f3e674c1e5a68c785ee0fa4
Rafael Prado <contato@rprado.design>
2020-12-15 16:04:43 -0300

    refactor: update challenge 4-3

--- src/4-3/src/teachers.js
+++ src/4-3/src/teachers.js
@@ -1,3 +1,7 @@
+// fake db
+const fs = require("fs");
+const data = require("../data.json");
+
 // create
 exports.post = function (req, res) {
   const keys = Object.keys(req.body);
@@ -8,7 +12,14 @@ exports.post = function (req, res) {
     }
   }
 
-  return res.send(req.body);
+  // import fake db
+  data.teachers.push(req.body);
+
+  // insert data into fake db
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err) {
+    if (err) return res.send("Deu erro para salvar.");
+    return res.redirect("/teachers");
+  });
 };
 
 // update
