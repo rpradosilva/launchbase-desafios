fe99bf6fded5c8635d9a34c2d293a091036402fa
Rafael Prado <contato@rprado.design>
2020-12-16 13:11:18 -0300

    refactor: challenge 4-3 complete

--- src/4-3/src/teachers.js
+++ src/4-3/src/teachers.js
@@ -12,8 +12,25 @@ exports.post = function (req, res) {
     }
   }
 
+  // organizando dados
+  let { avatar_url, name, born, graduate, lession, matter } = req.body;
+
+  // add data do registro
+  born = Date.parse(born);
+  const created_at = Date.now();
+  const id = Number(data.teachers.length + 1);
+
   // import fake db
-  data.teachers.push(req.body);
+  data.teachers.push({
+    id,
+    avatar_url,
+    name,
+    born,
+    graduate,
+    lession,
+    matter,
+    created_at,
+  });
 
   // insert data into fake db
   fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err) {
